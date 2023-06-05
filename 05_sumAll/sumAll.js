ba0e0451cef44eb51866689bd10ca0925d21932d
Mika Sato <mika99@live.com.au>
2023-06-05 16:11:15 +1000

    Solve 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+    let n;
+    if (num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    } else if (num1 < num2) {
+        for (n = num1; n <= num2; n++) {
+            finalSum += n;
+        }
+    } else {
+        for (n = num2; n <= num1; n++) {
+            finalSum += n;
+        }
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
