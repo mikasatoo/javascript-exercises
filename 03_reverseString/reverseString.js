82e9f244b5550740c5fa7e4f89ae4addf0e13364
Mika Sato <mika99@live.com.au>
2023-06-05 12:52:51 +1000

    Solve exercises 01 to 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let output = string.split("");  // split characters into an array
+    output.reverse();   // reverse order of elements in the array
+    return output.join("");    // join array elements back into a string
+    // could also join the above together in one line as: return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(myArray, myValue) {
+    let arrayLen = myArray.length;
+    for (let i = 0; i <= arrayLen; i++) {   // loop through array elements
+        if (myArray[i] === myValue) {   // if array element equal to myValue
+            myArray.splice(i, 1);     // remove one element from the specified position
+        }
+    }
+    return myArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
