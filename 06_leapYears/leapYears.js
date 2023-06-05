31d4d66a37fd737567f168b55e3f63f1d75fae66
Mika Sato <mika99@live.com.au>
2023-06-05 16:29:11 +1000

    Solve 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 === 0 && year % 400 === 0) {   // years divisible by 100 are not leap years unless they are divisible by 400
+            return true;
+        } else if (year % 100 !== 0) {   // other years divisible by 4 but not by 100 will be leap years
+            return true;
+        }
+    }
+    return false;
 };
 
 // Do not edit below this line
