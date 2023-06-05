5b5e5741d3de578b18c1c2ccee5c04f0e6abffea
Mika Sato <mika99@live.com.au>
2023-06-05 16:46:24 +1000

    Solve 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsiusTemp = (temp - 32)*(5/9);  // convert fahrenheit to celsius
+  if (celsiusTemp === 0) {
+    return celsiusTemp;   // return 0
+  } else {
+    return Number(celsiusTemp.toFixed(1));  // round to one decimal place and convert back to number (since toFixed() returns a string)
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheitTemp = (temp*(9/5)) + 32;  // convert celsius to fahrenheit
+  if (fahrenheitTemp === 0) {
+    return fahrenheitTemp;
+  } else {
+    return Number(fahrenheitTemp.toFixed(1));
+  }
 };
 
 // Do not edit below this line
