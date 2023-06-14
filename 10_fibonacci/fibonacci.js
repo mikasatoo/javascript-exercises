ad7d58d5613bbe25bb54c631267b0a6615244240
Mika Sato <mika99@live.com.au>
2023-06-14 16:50:46 +1000

    Write function to pass tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let arr = [1, 1];   // array with first two numbers in Fibonacci sequence
+    
+    if (n <= 0) return "OOPS";
 
+    for (let i = 2; i < n; i++) {
+        arr.push(arr[i - 2] + arr[i - 1]);
+    }
+
+    return arr[n - 1];
 };
 
 // Do not edit below this line
