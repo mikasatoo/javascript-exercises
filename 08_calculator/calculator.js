5e1edf607d9c7d9bdd4ec36adffaa1d7807c29e7
Mika Sato <mika99@live.com.au>
2023-06-14 15:44:32 +1000

    Write functions to pass tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return (a > b) ? (a - b) : (b - a);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, item) => {
+    return total + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, item) => {
+    return product *= item;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let output = 1;
+	for (i = a; i > 0; i--) {
+    output *= i;
+  }
+  return output;
 };
 
 // Do not edit below this line
